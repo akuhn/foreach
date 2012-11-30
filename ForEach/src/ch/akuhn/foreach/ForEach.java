fb2f472a14f27a8bf9c634aa5a61484bed1cc8cf
Adrian Kuhn <akuhn@iam.unibe.ch>
2012-11-30 01:36:56 -0800

    ForEach.partition

--- ForEach/src/ch/akuhn/foreach/ForEach.java
+++ ForEach/src/ch/akuhn/foreach/ForEach.java
@@ -4,119 +4,127 @@ import java.util.Arrays;
 
 public class ForEach {
 
-	public static <T>T result() {
+	public static <T> T result() {
 		return Query.result();
 	}
 
-	public static <T>Query<T,All<T>> all(Iterable<T> all) {
+	public static <T> Query<T,All<T>> all(Iterable<T> all) {
 		return Query.with(new All<T>(), all);
 	}
 
-	public static <T>Query<T,All<T>> all(T[] all) {
+	public static <T> Query<T,All<T>> all(T[] all) {
 		return Query.with(new All<T>(), all);
 	}
 
-	public static <T>Query<T,Any<T>> any(Iterable<T> all) {
+	public static <T> Query<T,Any<T>> any(Iterable<T> all) {
 		return Query.with(new Any<T>(), all);
 	}
 
-	public static <T>Query<T,Any<T>> any(T[] all) {
+	public static <T> Query<T,Any<T>> any(T[] all) {
 		return Query.with(new Any<T>(), all);
 	}
 
-	public static <T,K>Query<T,Collect<T,K>> collect(Class<K> key, Iterable<T> all) {
+	public static <T,K> Query<T,Collect<T,K>> collect(Class<K> key, Iterable<T> all) {
 		return Query.with(new Collect<T,K>(), all);
 	}
 
-	public static <T,K>Query<T,Collect<T,K>> collect(Class<K> key, T[] all) {
+	public static <T,K> Query<T,Collect<T,K>> collect(Class<K> key, T[] all) {
 		return Query.with(new Collect<T,K>(), all);
 	}
 
-	public static <T>Query<T,Collect1<T>> collect1(Iterable<T> all) {
+	public static <T> Query<T,Collect1<T>> collect1(Iterable<T> all) {
 		return Query.with(new Collect1<T>(), all);
 	}
 
-	public static <T>Query<T,Collect1<T>> collect1(T[] all) {
+	public static <T> Query<T,Collect1<T>> collect1(T[] all) {
 		return Query.with(new Collect1<T>(), all);
 	}
 
-	public static <T>Query<T,Count<T>> count(Iterable<T> all) {
+	public static <T> Query<T,Count<T>> count(Iterable<T> all) {
 		return Query.with(new Count<T>(), all);
 	}
 
-	public static <T>Query<T,Count<T>> count(T[] all) {
+	public static <T> Query<T,Count<T>> count(T[] all) {
 		return Query.with(new Count<T>(), all);
 	}
 
-	public static <T>Query<T,Detect<T>> detect(Iterable<T> all) {
+	public static <T> Query<T,Detect<T>> detect(Iterable<T> all) {
 		return Query.with(new Detect<T>(), all);
 	}
 
-	public static <T>Query<T,Detect<T>> detect(T[] all) {
+	public static <T> Query<T,Detect<T>> detect(T[] all) {
 		return Query.with(new Detect<T>(), all);
 	}
 
-	public static <T>Query<Pair<T,T>,Fold<T>> fold(Iterable<T> all) {
+	public static <T> Query<Pair<T,T>,Fold<T>> fold(Iterable<T> all) {
 		return Query.with(new Fold<T>(), Iterators.pairs(all));
 	}
 
-	public static <T>Query<Pair<T,T>,Fold<T>> fold(T[] all) {
+	public static <T> Query<Pair<T,T>,Fold<T>> fold(T[] all) {
 		return ForEach.fold(Arrays.asList(all));
 	}
 
-	public static <T>Query<T,GroupBy<T>> groupBy(Iterable<T> all) {
+	public static <T> Query<T,GroupBy<T>> groupBy(Iterable<T> all) {
 		return Query.with(new GroupBy<T>(), all);
 	}
 
-	public static <T>Query<T,GroupBy<T>> groupBy(T[] all) {
+	public static <T> Query<T,GroupBy<T>> groupBy(T[] all) {
 		return Query.with(new GroupBy<T>(), all);
 	}
 
-	public static <T>Query<T,IndexOf<T>> indexOf(Iterable<T> all) {
+	public static <T> Query<T,IndexOf<T>> indexOf(Iterable<T> all) {
 		return Query.with(new IndexOf<T>(), all);
 	}
 
-	public static <T>Query<T,IndexOf<T>> indexOf(T[] all) {
+	public static <T> Query<T,IndexOf<T>> indexOf(T[] all) {
 		return Query.with(new IndexOf<T>(), all);
 	}
 
-	public static <T,R>Query<T,Inject<T,R>> inject(R seed, Iterable<T> all) {
+	public static <T,R> Query<T,Inject<T,R>> inject(R seed, Iterable<T> all) {
 		return Query.with(new Inject<T,R>(seed), all);
 	}
 
-	public static <T,R>Query<T,Inject<T,R>> inject(R seed, T[] all) {
+	public static <T,R> Query<T,Inject<T,R>> inject(R seed, T[] all) {
 		return Query.with(new Inject<T,R>(seed), all);
 	}
 
-	public static <T>Query<T,Reject<T>> reject(Iterable<T> all) {
+	public static <T> Query<T,Partition<T>> partition(Iterable<T> all) {
+		return Query.with(new Partition<T>(), all);
+	}
+
+	public static <T> Query<T,Partition<T>> partition(T[] all) {
+		return Query.with(new Partition<T>(), all);
+	}
+
+	public static <T> Query<T,Reject<T>> reject(Iterable<T> all) {
 		return Query.with(new Reject<T>(), all);
 	}
 
-	public static <T>Query<T,Reject<T>> reject(T[] all) {
+	public static <T> Query<T,Reject<T>> reject(T[] all) {
 		return Query.with(new Reject<T>(), all);
 	}
 
-	public static <T>Query<T,Select<T>> select(Iterable<T> all) {
+	public static <T> Query<T,Select<T>> select(Iterable<T> all) {
 		return Query.with(new Select<T>(), all);
 	}
 
-	public static <T>Query<T,Select<T>> select(T[] all) {
+	public static <T> Query<T,Select<T>> select(T[] all) {
 		return Query.with(new Select<T>(), all);
 	}
 
-	public static <T>Query<Pair<T,T>,Split<T>> split(Iterable<T> all) {
+	public static <T> Query<Pair<T,T>,Split<T>> split(Iterable<T> all) {
 		return Query.with(new Split<T>(), Iterators.pairs(all));
 	}
 
-	public static <T>Query<Pair<T,T>,Split<T>> split(T[] all) {
+	public static <T> Query<Pair<T,T>,Split<T>> split(T[] all) {
 		return ForEach.split(Arrays.asList(all));
 	}
 
-	public static <T>Query<T,Sum<T>> sum(Iterable<T> all) {
+	public static <T> Query<T,Sum<T>> sum(Iterable<T> all) {
 		return Query.with(new Sum<T>(), all);
 	}
 
-	public static <T>Query<T,Sum<T>> sum(T[] all) {
+	public static <T> Query<T,Sum<T>> sum(T[] all) {
 		return Query.with(new Sum<T>(), all);
 	}
 
---
+++ ForEach/src/ch/akuhn/foreach/Partition.java
@@ -0,0 +1,58 @@
+package ch.akuhn.foreach;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class Partition<Each> extends For<Each> {
+
+	public Each value;
+	public boolean yield;
+
+	private List<Each> select;
+	private List<Each> reject;
+
+	@Override
+	protected void beforeLoop() {
+		select = new ArrayList();
+		reject = new ArrayList();
+	}
+
+	@Override
+	protected void beforeEach(Each each) {
+		this.value = each;
+		this.yield = false;
+	}
+
+	@Override
+	protected void afterEach() throws Abortion {
+		(yield ? select : reject).add(value);
+	}
+
+	@Override
+	protected Object afterLoop() {
+		return Pair.of(select, reject);
+	}
+
+	public static <E> List<E> selected() {
+		return ((Pair<List<E>,?>) ForEach.result()).fst;
+	}
+
+	public static <E> List<E> rejected() {
+		return ((Pair<?,List<E>>) ForEach.result()).snd;
+	}
+
+	@Test
+	public void shouldPartitionByLength() {
+		String[] words = "The quick brown fox jumps over the lazy dog".split(" ");
+		for (Partition<String> each: ForEach.partition(words)) {
+			each.yield = each.value.length() < 4;
+		}
+		assertEquals("[The, fox, the, dog]", Partition.selected().toString());
+		assertEquals("[quick, brown, jumps, over, lazy]", Partition.rejected().toString());
+	}
+
+}
